d84af031ff7b0b65a29c301b8691bd89e8620d2b
jeanevitoria <131841615+jeanevitoria@users.noreply.github.com>
2023-04-29 01:44:03 -0300

    Delete exercicio directory

--- exercicio/src/edu/jeane/exercicios/App.java
+++
@@ -1,8 +0,0 @@
-package edu.jeane.exercicios;
-public class App {
-    public static void main(String[] args) {
-        for (int i = 150; i <= 300; i++) {
-            System.out.println ("meu pai amado");
-        }
-    }
-}
