253a2f65d1b83c4f3cf2f96a3f6be43d83fd3a2d
jeanevitoria <131841615+jeanevitoria@users.noreply.github.com>
2023-04-29 01:35:25 -0300

    Add files via upload

---
+++ exercicio/src/edu/jeane/exercicios/App.java
@@ -0,0 +1,8 @@
+package edu.jeane.exercicios;
+public class App {
+    public static void main(String[] args) {
+        for (int i = 150; i <= 300; i++) {
+            System.out.println ("meu pai amado");
+        }
+    }
+}
